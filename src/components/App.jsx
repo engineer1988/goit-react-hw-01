6f1724d2ded04bd545a0b8beea926f2ea75d0fca
Dmytro Maniuta <ip722428@gmail.com>
2024-03-13 16:45:27 +0200

    finished task-1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,6 @@
 import "./App.css";
 import userData from "../userData.json";
-import Profile from "./Profile";
+import Profile from "./Profile/Profile";
 
 export default function App() {
   return (
