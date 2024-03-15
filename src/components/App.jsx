9f5412f280b95abaa680a579ec6583f4adc17508
Dmytro Maniuta <ip722428@gmail.com>
2024-03-15 17:45:55 +0200

    fixed mistakes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,7 +2,7 @@ import "./App.css";
 import userData from "../userData.json";
 import friends from "../friends.json";
 import transactions from "../transactions.json";
-import Profile from "./profile/Profile";
+import Profile from "./test/Profile";
 import FriendList from "./friendList/FriendList";
 import TransactionHistory from "./transactionHistory/TransactionHistory";
 
